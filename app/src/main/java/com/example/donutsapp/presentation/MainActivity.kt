f2039d4c34b0190b1c227ee50b5222fc38cf94a2
Abubakir <abubakir@macbook-air-abubakir.local>
2023-12-04 18:20:31 +0600

    Initial commit

--- app/src/main/java/com/example/donutsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/donutsapp/presentation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.donutsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.animeapp.R
 import com.example.animeapp.databinding.ActivityMainBinding
 
@@ -10,9 +12,12 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by  lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        navController = findNavController(R.id.nav_host_fragment)
+
     }
 }
